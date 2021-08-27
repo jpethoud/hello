897f504b7e3788c885be9292cb92e77478b65405
Jace Pethoud <jpethoud3@huskers.unl.edu>
2021-08-27 02:42:22 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Jace Pethoud
+ *         jpethoud3@huskers.unl.edu
+ * Date: 2021/08/24
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Jace Pethoud\n");
+
+  return 0;
+}
